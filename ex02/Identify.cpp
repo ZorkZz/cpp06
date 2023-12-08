ab9d92eff68fa2f3ea3a6a1c6ff26d9d2158a57d
zorkz <alexandre.stachnik2@gmail.com>
2023-12-08 14:05:19 +0100

    ex02

---
+++ ex02/Identify.cpp
@@ -0,0 +1,52 @@
+#include "Base.hpp"
+
+Base	*generate()
+{
+	srand(time(NULL));
+	char c = "ABC"[rand() % 3];
+
+	if (c == 'A')
+		return (new A);
+	else if (c == 'B')
+		return (new B);
+	return (new C);
+}
+
+void	identify(Base *base)
+{
+	if (dynamic_cast<A*>(base))
+		std::cout << "p: A" << std::endl;
+	else if (dynamic_cast<B*>(base))
+		std::cout << "p: B" << std::endl;
+	else if (dynamic_cast<C*>(base))
+		std::cout << "p: C" << std::endl;
+}
+
+void	identify(Base &base)
+{
+	try
+	{
+		dynamic_cast<A&>(base);
+		std::cout << "r: A" << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		try
+		{
+			dynamic_cast<B&>(base);
+			std::cout << "r: B" << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			try
+			{
+				dynamic_cast<C&>(base);
+				std::cout << "r: C" << std::endl;
+			}
+			catch(std::exception &e)
+			{
+				std::cout << "Error: " << std::endl;
+			}
+		}
+	}
+}
